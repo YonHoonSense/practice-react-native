842438ab4ee6236046e33f87c705f81a74c70e6d
YonHoonSense <yh0520@naver.com>
2019-06-30 18:56:51 +0900

    class #6

---
+++ ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet} from "react-native";
+
+class ToDo extends React.Component{
+	render(){
+		<View>
+			<Text>ToDo</Text>
+		</View>
+
+
+	}
+}
+
+const styles = StyleSheet.create({})
