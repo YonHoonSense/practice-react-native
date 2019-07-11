1402eae4217edcd91f4159cc4637ebb6efd72360
YonHoonSense <yh0520@naver.com>
2019-07-11 21:28:43 +0900

    save files

--- App.js
+++ App.js
@@ -7,16 +7,27 @@ import { StyleSheet,
 	Dimensions,
 	Platform,
  	ScrollView } from 'react-native';
+import { AppLoading } from 'expo';
 import ToDo from 'ToDo';
+import uuidv1 from 'uuid/v1';
 
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
 	state = {
-		newToDo: ""
+		newToDo: "",
+		loadedToDos: false,
+		toDos: {}
 	};
+	componentDidMount = () => {
+		this._loadTodos();
+	}
 	render(){
-		const {newToDo} = this.state;
+		const { newToDo, loadedToDos, toDos } = this.state;
+		console.log(toDos);
+		if(!loadedToDos) {
+			return <AppLoading />;
+		}
   	return (
     	<View style={styles.container}>
       	<StatusBar barStyle="light-content" />
@@ -30,9 +41,10 @@ export default class App extends React.Component {
 						placeholderTextColor={"#999"}
 						returnKeyType={"done"}
 						autoCorrect={false}
+						onSubmitEditing={this._addToDo}
 					/>
 					<ScrollView contentContainerStyle={styles.toDos}>
-						<ToDo />
+						{Object.values(toDos).map(toDo => <ToDo key={toDo.id} {...toDo}/>)}
 					</ScrollView>
 				</View>
     	</View>
@@ -42,6 +54,36 @@ export default class App extends React.Component {
 		this.setState({
 			newToDo:text
 		});
+	};
+	_loadTodos = () => {
+		this.setState({
+			loadedToDos:true
+		});
+	};
+	_addToDo = () => {
+		const { newToDo } = this.state;
+		if(newToDo !== ""){
+			this.setState(prevState => {
+				const ID = uuidv1();
+				const newToDoObject = {
+					[ID]: {
+						id: ID,
+						isCompleted: false,
+						text: newToDo,
+						createdAt: Date.now()
+					}
+				};
+				const newState = {
+					...prevState,
+					newToDo: "",
+					toDos: {
+						...prevState.toDos,
+						...newToDoObject
+					}
+				};
+				return { ...newState };
+			});
+		}
 	};
 }
 
